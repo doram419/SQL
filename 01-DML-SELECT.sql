a37fc0852c8f26277dbf16d47d2e914963aca761
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-04-24 12:03:01 +0900

    Update 01-DML-SELECT.sql
    12:02

--- 01-DML-SELECT.sql
+++ 01-DML-SELECT.sql
@@ -38,6 +38,22 @@ SELECT employee_id,
 -- 특정 테이블의 값이 아닌 시스템으로부터 데이터를 받아오고자 할때 : dual(가상 테이블)
 select 3.14159 * 10 * 10 from dual;
 
+-- 특정 컬럼의 값 산술 연산에 포함시키고자 할 경우
+select first_name,
+    salary,
+    salary * 12
+    FROM employees;
+    
+-- 다음 문장을 실행해봅시다.
+select first_name,
+    job_id,
+    job_id * 12
+    FROM employees;
+-- 오류의 원인 : job_id 는 문자열(VARCHAR2),
+--             문자열을 산술 계산 할 수 없음
+
+desc employees;
+
 -- NULL
 -- 이름, 급여, 커미션 비율를 츨력해보자
 Select first_name, salary, commission_pct 
@@ -67,19 +83,34 @@ From employees;
 -- > 컬럼명 뒤에 as 별칭
 -- !! 표시명에 특수문자가 포함된 경우 " " 로 묶어서 표현하기
 
--- 특정 컬럼의 값 산술 연산에 포함시키고자 할 경우
-select first_name,
-    salary,
-    salary * 12
-    FROM employees;
-    
--- 다음 문장을 실행해봅시다.
-select first_name,
-    job_id,
-    job_id * 12
-    FROM employees;
--- 오류의 원인 : job_id 는 문자열(VARCHAR2),
---             문자열을 산술 계산 할 수 없음
+-- 예제1 : 직원 아이디, 이름, 급여를 출력하자
+--        직원 아이디는 empNO, 이름은 f-name, 급여는 월 급으로 표시하라.
+Select employee_id empNo,
+    first_name "f-name",
+    salary as "월 급"
+From employees;
 
-desc employees;
+-- 예제2 : first_name과 last_name을 합쳐서 Full Name이라고 출력하고
+--        commission_pct가 포함된 salary를 급여라고 출력하며,
+--        급여 * 12를 하여 연봉으로 출력해라
+Select first_name || ' ' || last_name as "Full Name",
+-- 오라클에서는 자바처럼 + 연산을 문자에서 사용할 수 없다!
+       salary + salary * NVL(commission_pct, 0) as "급여(커미션 포함)",
+       salary * 12 as "연봉"
+       From employees;
+       
+-- [연습 예제] hr.employees 전체 튜플에 다음과 같이 Column Alias를 붙여 출력해 봅니다
+--  이름 : first_name last_name
+--  입사일: hire_date
+--  전화번호 : phone_number
+--  급여 : salary
+--  연봉 : salary * 12
+Select first_name || ' ' || last_name as "이름",
+       hire_date as "입사일",
+       phone_number as "전화번호",
+       salary as "급여",
+       salary * 12 as "연봉"
+       From employees;
+
+
 
