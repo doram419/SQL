93fdc82db5dfe95f8aed24489ff461ebc01bb136
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-04-30 10:21:09 +0900

    Update 02-DML-AGGREGATION.sql
    10:21

--- 02-DML-Join/02-DML-AGGREGATION.sql
+++ 02-DML-Join/02-DML-AGGREGATION.sql
@@ -514,3 +514,36 @@ Where
 Order By
     hire_date Asc,
     salary Asc;
+    
+-- 다중행 서브 쿼리
+-- 서브 쿼리 결과가 둘 이상의 레코드일 때 단일행 비교연산자는 사용할 수 없다.
+-- 집합 연산에 관련된 In, Any, All, Exists 등을 사용해야 한다.
+
+-- 예제) 직원들 중,
+-- 110번 부서 사람들이 받는 급여와 같은 급여를 받는 직원들의 목록
+-- 서브 쿼리부터 알아보자!
+-- A1. 110번 부서의 사람들은 얼마나 받는가? (12008, 8300)
+Select 
+    salary
+From employees
+Where department_id = 110;
+
+-- A2. 급여가 12008, 8300인 직원 목록
+Select 
+    first_name,
+    salary
+From employees
+Where salary In (12008, 8300);
+
+-- A3. 110번 부서가 가지고 있는 급여의 목록과 하나라도 일치하는 사람의 이름과 급여
+Select 
+    first_name,
+    salary
+From employees
+Where salary In (Select salary
+    From employees
+    Where department_id = 110);
+    
+
+
+
