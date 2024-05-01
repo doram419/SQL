7bdac4a1b8eb05cabc7e9364c7e55c0cf2cedd23
JiHwan Kim <57897955+doram419@users.noreply.github.com>
2024-05-01 09:40:16 +0900

    Update DBian-5-3-Litteral.sql
    letteral always return same value

--- DBian-5-3-Litteral.sql
+++ DBian-5-3-Litteral.sql
@@ -7,4 +7,13 @@ SELECT
 FROM DUAL;
 -- 6번째 열은 Alias를 지칭하지 않아 참조할 수 없는 열
 
--- 인용방식 문자 리터럴
+-- 인용방식 문자 리터럴 사용 가능
+-- [],{},<>,() 사용 가능
+SELECT q'[2'b]' AS c1,
+       q'{[3c]}' AS c2
+FROM DUAL;
+
+-- 리터럴은 전체 행에서 동일한 값을 반환한다.
+SELECT 'Department' AS c1,
+       department_id 
+FROM departments;
